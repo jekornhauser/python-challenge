49ce07254045ea4974d1bac2d280797fcf96afb0
jekornhauser <jekornhauser@gmail.com>
2020-11-14 21:04:57 -0500

    file setup git status git log $ git log

---
+++ PyBank/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 20:49:45 2020
+
+@author: jekor
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 20:50:14 2020
+
+@author: jekor
+"""
+
